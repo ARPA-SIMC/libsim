e6cbe16796855509556aa38700c42dff79aa8019
Davide Cesari <dcesari@arpa.emr.it>
2007-07-04 14:57:08 +0000

    Aggiunto il modulo vol7d_class_cumulate che fornisce alla classe vol7d dei semplici metodi di elaborazione, per ora solo la ricumulazione di dati reali su un intervallo specificato
    git-svn-id: https://svn.smr.arpa.emr.it/vol7d/trunk@68 500a5620-302c-0410-bb8c-d54761bbe776

--- examples/esempio_v7doracle_multi.f90
+++ examples/esempio_v7doracle_multi.f90
@@ -1,41 +1,88 @@
 PROGRAM v7doracle
 ! Programma di esempio di estrazione dall'archivio Oracle del SIM
 USE datetime_class
-USE vol7d_oraclesim_class
 USE vol7d_class
+USE vol7d_class_compute
+USE vol7d_oraclesim_class
 
 IMPLICIT NONE
 
 TYPE(vol7d_oraclesim) :: v7d
 TYPE(vol7d_network) :: dummy_network
-TYPE(datetime) :: ti, tf
+TYPE(datetime) :: ti, tf, tc
+TYPE(vol7d) :: vol_cumh, vol_cumd
+TYPE(timedelta) :: dt_cum
 CHARACTER(len=12) :: c
-INTEGER :: i, n
-REAL, POINTER :: vol2d(:,:)
+INTEGER :: i, j, n
+REAL, POINTER :: vol2d(:,:), vol2d_cum(:,:)
+INTEGER :: un
+CHARACTER(len=512) :: filesim
+TYPE(geo_coordvect),POINTER :: macroa(:)
+INTEGER, ALLOCATABLE :: in_macroa(:)
 
 ! Definisco le date iniziale e finale
-CALL init(ti, year=2007, month=3, day=19, hour=12)
+CALL init(ti, year=2007, month=3, day=18, hour=12)
 CALL init(tf, year=2007, month=3, day=21, hour=00)
+! Definisco una rete fittizia in cui forzare i dati
 CALL init(dummy_network, 1)
 ! Chiamo il costruttore della classe vol7d_oraclesim per il mio oggetto
 CALL init(v7d)
 ! Importo i dati, variabile 'B13011' della btable (precipitazione),
-! rete 18 (FIDUPO), 20 (SIMNBO), 21 (SIMNPR)
-CALL import(v7d, 'B13011', 18, ti, tf, set_network=dummy_network)
-PRINT*,SHAPE(v7d%vol7d%voldatir)
-CALL import(v7d, 'B13011', (/20,21/), ti, tf, set_network=dummy_network)
-PRINT*,SHAPE(v7d%vol7d%voldatir)
-! Creo una vista su un array bidimensionale che scorre le dimensioni
-! dell'anagrafica e del tempo (vol7d_ana_d, vol7d_time_d)
-CALL vol7d_get_voldatir(v7d%vol7d, (/vol7d_ana_d,vol7d_time_d/), vol2dp=vol2d)
-! Calcolo la media e la stampo assieme all'istante
-DO i = 1, SIZE(v7d%vol7d%time)
-  CALL getval(v7d%vol7d%time(i), oraclesimdate=c)
-  n = COUNT (vol2d(:,i) /= rmiss)
+! per un vettore di reti mettendo tutto in un'unica rete
+CALL import(v7d, 'B13011', (/11,12,13,15,17,18,19,20,21,22/), &
+ ti, tf, set_network=dummy_network)
+
+! Cumulo i dati su intervalli orari
+CALL init(dt_cum, hour=1)
+CALL vol7d_cumulate(v7d%vol7d, vol_cumh, dt_cum)
+! Mi faccio dare una "vista" bidimensionale dei miei dati
+CALL vol7d_get_voldatir(vol_cumh, (/vol7d_ana_d,vol7d_time_d/), vol2dp=vol2d_cum)
+! Stampo la media su tutte le stazioni ora per ora
+DO i = 1, SIZE(vol_cumh%time)
+  CALL getval(vol_cumh%time(i), oraclesimdate=c)
+  n = COUNT (vol2d_cum(:,i) /= rmiss)
   IF (n > 0) THEN
-    PRINT*, c, ' prec. media:', SUM(vol2d(:,i), mask=(vol2d(:,i) /= rmiss))/n
+    PRINT'(2A,G12.5)',c,' prec. media (mm): ', &
+     SUM(vol2d_cum(:,i), mask=(vol2d_cum(:,i) /= rmiss))/n
   ENDIF
 ENDDO
 
+! Cumulo i dati su intervalli giornalieri
+CALL init(dt_cum, day=1)
+CALL init(tc, year=2007, month=3, day=17, hour=00)
+CALL vol7d_cumulate(vol_cumh, vol_cumd, dt_cum, tc)
+! Mi faccio dare una "vista" bidimensionale dei miei dati
+CALL vol7d_get_voldatir(vol_cumd, (/vol7d_ana_d,vol7d_time_d/), vol2dp=vol2d_cum)
+
+! Importo un file con le macroaree Emilia Romagna
+un = open_package_file('polipciv4.dat', filetype_data)
+IF (un < 0) STOP 1
+INQUIRE(unit=un, name=filesim)
+CLOSE(un)
+CALL import(macroa, shpfilesim=filesim)
+ALLOCATE(in_macroa(SIZE(vol_cumd%ana)))
+in_macroa = 0
+! Determino l'appartenenza delle stazioni alle macroaree
+DO i = 1, SIZE(vol_cumd%ana)
+  DO j = 1, SIZE(macroa)
+    IF (geo_coordvect_inside(macroa(j), vol_cumd%ana(i)%coord)) THEN
+      in_macroa(i) = j
+      CYCLE
+    ENDIF
+  ENDDO
+ENDDO
+
+! Stampo la media su tutte le stazioni di ogni macroarea giorno per giorno
+DO i = 1, SIZE(vol_cumd%time)
+  CALL getval(vol_cumd%time(i), oraclesimdate=c)
+  DO j = 1, SIZE(macroa)
+    n = COUNT(vol2d_cum(:,i) /= rmiss .AND. in_macroa(:) == j)
+    IF (n > 0) THEN
+      PRINT'(2A,I3,A,G12.5)',c,' macroarea: ',j,' prec media (mm): ', &
+       SUM(vol2d_cum(:,i), mask=(vol2d_cum(:,i) /= rmiss &
+       .AND. in_macroa(:) == j))/n
+    ENDIF
+  ENDDO
+ENDDO
 
 END PROGRAM v7doracle
