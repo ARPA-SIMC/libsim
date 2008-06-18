7acf903abd33a44b98743e9bca0333b7a70a0334
Davide Cesari <dcesari@arpa.emr.it>
2008-06-18 14:39:31 +0000

    Aggiunte funzioni per la gestione interpoerativa di variabili caratter tra Fortran e C
    git-svn-id: https://svn.smr.arpa.emr.it/vol7d/trunk@258 500a5620-302c-0410-bb8c-d54761bbe776

--- base/char_utilities.f90
+++ base/char_utilities.f90
@@ -12,6 +12,7 @@
 !! \endcode
 !! \ingroup base
 MODULE char_utilities
+USE kinds
 IMPLICIT NONE
 
 PRIVATE int_to_char
@@ -34,7 +35,7 @@ END INTERFACE
 CONTAINS
 
 !> Variante con input intero
-elemental FUNCTION int_to_char(i, form)
+ELEMENTAL FUNCTION int_to_char(i, form)
 INTEGER,INTENT(in) :: i !< valore da rappresentare in CHARACTER
 CHARACTER(len=*),INTENT(in),OPTIONAL :: form !< formato opzionale
 
@@ -47,5 +48,43 @@ ELSE
 ENDIF
 
 END FUNCTION int_to_char
+
+!> Converte una variabile \a CHARACTER in una stringa passabile ad una
+!! funzione scritta in linguaggio C come \a char* .
+FUNCTION fchar_to_cstr(fchar, pcstr) RESULT(cstr)
+CHARACTER(len=*), INTENT(in) :: fchar !< variabile da convertire
+INTEGER(kind=int_b), POINTER, OPTIONAL :: pcstr(:) !< puntatore opzionale che può essere allocato per contenere il risultato, dovrà essere deallocato a carico del programma chiamante
+INTEGER(kind=int_b) :: cstr(LEN(fchar)+1)
+
+cstr(1:LEN(fchar)) = TRANSFER(fchar, cstr, LEN(fchar))
+cstr(LEN(fchar)+1) = 0 ! Termino con zero
+IF (PRESENT(pcstr)) THEN
+  ALLOCATE(pcstr(LEN(fchar)+1))
+  pcstr = cstr
+ENDIF
+
+END FUNCTION fchar_to_cstr
+
+
+!> Converte una stringa passabile ad una funzione scritta in linguaggio C
+!! come \a char* in una variabile \a CHARACTER
+FUNCTION cstr_to_fchar(cstr) RESULT(fchar)
+INTEGER(kind=int_b), INTENT(in) :: cstr(:) !< variabile da convertire
+!CHARACTER(len=SIZE(cstr)-1) :: char
+CHARACTER(len=SIZE(cstr)-1) :: fchar
+
+INTEGER :: i
+
+!l = MIN(LEN(char), SIZE(cstr)-1)
+fchar = TRANSFER(cstr(1:SIZE(cstr)-1), fchar)
+DO i = 1, SIZE(cstr)-1
+  IF (fchar(i:i) == CHAR(0)) THEN ! se e` 'null-terminated' riempio con spazi
+    fchar(i:) = ' '
+    EXIT
+  ENDIF
+ENDDO
+
+END FUNCTION cstr_to_fchar
+
 
 END MODULE char_utilities
