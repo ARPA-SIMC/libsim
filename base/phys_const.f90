8f50eede21b1e23a6823d34195b1a85416913c46
Paolo Patruno <ppatruno@arpa.emr.it>
2008-12-21 17:09:56 +0000

    doubleprecision constants added
    git-svn-id: https://svn.smr.arpa.emr.it/vol7d/trunk@368 500a5620-302c-0410-bb8c-d54761bbe776

--- base/phys_const.f90
+++ base/phys_const.f90
@@ -27,3 +27,24 @@ REAL, PARAMETER :: cvd = cpd-rd !< calore specifico dell'aria secca a volume cos
 REAL, PARAMETER :: lvw = 2.5E+6 !< calore latente...?
 
 END MODULE phys_const
+
+MODULE doubleprecision_phys_const
+IMPLICIT NONE
+
+! pi = 4.*ATAN(1.)
+DOUBLEPRECISION, PARAMETER :: pi = 3.14159 !< pi greco (\f$\pi\f$)
+DOUBLEPRECISION, PARAMETER :: rearth = 6370997. !< raggio medio della Terra (\f$m\f$)
+DOUBLEPRECISION, PARAMETER :: gearth = 9.81 !< accelerazione gravitazionale media alla superficie terrestre (\f$m/s^2\f$)
+DOUBLEPRECISION, PARAMETER :: omearth = 2*pi/86164. !< velocità angolare terrestre (\f$s^{-1}\f$)
+DOUBLEPRECISION, PARAMETER :: degrad = pi/180. !< fattore di conversione da gradi a radianti
+DOUBLEPRECISION, PARAMETER :: raddeg = 180./pi !< fattore di conversione da radianti a gradi
+DOUBLEPRECISION, PARAMETER :: rd = 287.05 !< costante dei gas per l'aria secca
+DOUBLEPRECISION, PARAMETER :: rv = 461.51 !< costante dei gas per il vapore acqueo
+DOUBLEPRECISION, PARAMETER :: eps0 = rd/rv !< \f$rd/rv\f$
+DOUBLEPRECISION, PARAMETER :: epsy = rv/rd-1. !< \f$rv/rd -1\f$
+DOUBLEPRECISION, PARAMETER :: rcp = 2./7. !< rapporto tra calore specifico...?
+DOUBLEPRECISION, PARAMETER :: cpd = rd/rcp !< calore specifico dell'aria secca a pressione costante
+DOUBLEPRECISION, PARAMETER :: cvd = cpd-rd !< calore specifico dell'aria secca a volume costante
+DOUBLEPRECISION, PARAMETER :: lvw = 2.5E+6 !< calore latente...?
+
+END MODULE doubleprecision_phys_const
