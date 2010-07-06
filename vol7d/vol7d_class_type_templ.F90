5cbce0eece5b6bb2a12e0d96255699d7a141c8ab
Paolo Patruno <ppatruno@arpa.emr.it>
2010-07-06 11:11:42 +0000

    license everywhere
    git-svn-id: https://svn.smr.arpa.emr.it/vol7d/trunk@719 500a5620-302c-0410-bb8c-d54761bbe776

--- vol7d/vol7d_class_type_templ.F90
+++ vol7d/vol7d_class_type_templ.F90
@@ -1,3 +1,20 @@
+! Copyright (C) 2010  ARPA-SIM <urpsim@smr.arpa.emr.it>
+! authors:
+! Davide Cesari <dcesari@arpa.emr.it>
+! Paolo Patruno <ppatruno@arpa.emr.it>
+
+! This program is free software; you can redistribute it and/or
+! modify it under the terms of the GNU General Public License as
+! published by the Free Software Foundation; either version 2 of 
+! the License, or (at your option) any later version.
+
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+
+! You should have received a copy of the GNU General Public License
+! along with this program.  If not, see <http://www.gnu.org/licenses/>.
 SUBROUTINE vol7d_merge_final/**/VOL7D_POLY_TYPES(this, that, v7dtmp, &
  remapa1, remapa2, remapt1, remapt2, remapl1, remapl2, &
  remaptr1, remaptr2, remapn1, remapn2)
--- vol7d/vol7d_dballe_class_nana.f90
+++ vol7d/vol7d_dballe_class_nana.f90
@@ -1,3 +1,20 @@
+! Copyright (C) 2010  ARPA-SIM <urpsim@smr.arpa.emr.it>
+! authors:
+! Davide Cesari <dcesari@arpa.emr.it>
+! Paolo Patruno <ppatruno@arpa.emr.it>
+
+! This program is free software; you can redistribute it and/or
+! modify it under the terms of the GNU General Public License as
+! published by the Free Software Foundation; either version 2 of 
+! the License, or (at your option) any later version.
+
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+
+! You should have received a copy of the GNU General Public License
+! along with this program.  If not, see <http://www.gnu.org/licenses/>.
 !type /**/VOL7D_POLY_TYPES_V
 
 if (associated(this%vol7d%anavar%/**/VOL7D_POLY_TYPES_V))then
