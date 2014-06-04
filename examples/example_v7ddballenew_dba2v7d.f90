c154050d24f632832edeaf19798a908269a91951
Paolo Patruno <ppatruno@arpa.emr.it>
2014-06-04 17:41:54 +0000

    a working new import from dba to vol7d
    git-svn-id: https://svn.smr.arpa.emr.it/vol7d/trunk@1374 500a5620-302c-0410-bb8c-d54761bbe776

--- examples/example_v7ddballenew_dba2v7d.f90
+++ examples/example_v7ddballenew_dba2v7d.f90
@@ -17,6 +17,7 @@
 ! along with this program.  If not, see <http://www.gnu.org/licenses/>.
 PROGRAM v7ddballe_dba2v7d
 ! Programma di esempio di estrazione e scrittura dall'archivio DB-all.e
+use dballe_class
 USE datetime_class
 USE log4fortran
 USE vol7d_class
@@ -25,7 +26,11 @@ USE vol7d_dballenew_class
 IMPLICIT NONE
 
 TYPE(vol7d_dballe) :: v7d_dba
-integer :: category,ier
+TYPE(vol7d) :: v7d
+type(dbametaanddata),allocatable :: metaanddatav(:)
+type(dbadcv) :: attrv
+
+integer :: category,ier,i
 character(len=512):: a_name,filename="/tmp/example.bufr"
 !"../data/example_temp.bufr"
 
@@ -34,6 +39,10 @@ call l4f_launcher(a_name)
 !init di log4fortran
 ier=l4f_init()
 call l4f_category_log(category,L4F_INFO,"inizio")
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!                           import from file
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 ! Chiamo il costruttore della classe vol7d_dballe per il mio oggetto in export
 CALL init(v7d_dba,filename=filename,file=.true.,categoryappend="dballenewapi")
@@ -50,6 +59,70 @@ call display(v7d_dba%vol7d)
 call l4f_category_log(category,L4F_INFO,"fine import")
 
 CALL delete (v7d_dba) 
+
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!                           import from metaanddatav
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+allocate(metaanddatav(2))   ! one metadata for data and one for constant data
+
+metaanddatav(1)%metadata=dbametadata( &
+  level=dbalevel(level1=105, l1=2000) &
+ ,timerange=dbatimerange(timerange=4, p1=3600,p2=7200) &
+ ,ana=dbaana(lon=10.d0,lat=45.d0) &
+ ,network=dbanetwork("generic") &
+ ,datetime=dbadatetime(datetime_new(2014,01,06,18,00)))
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! create  an etherogeneous ensamble of data
+allocate (metaanddatav(1)%dataattrv%dataattr(2))
+
+! first data
+allocate (metaanddatav(1)%dataattrv%dataattr(1)%dat,source=dbadatai("B13003",85))
+
+! create an etherogeneous ensamble of attr
+allocate (attrv%dcv(3))
+allocate (attrv%dcv(1)%dat,source=dbadatar("*B33192",30.))
+allocate (attrv%dcv(2)%dat,source=dbadatai("*B33193",50))
+allocate (attrv%dcv(3)%dat,source=dbadatar("*B33194",70.))
+!assemble data and attribute
+metaanddatav(1)%dataattrv%dataattr(1)%attrv=attrv
+
+! second data
+allocate (metaanddatav(1)%dataattrv%dataattr(2)%dat,source=dbadatai("B12101",27315))
+! create an etherogeneous ensamble of attr
+deallocate(attrv%dcv)
+allocate (attrv%dcv(2))
+allocate (attrv%dcv(1)%dat,source=dbadatar("*B33192",30.))
+allocate (attrv%dcv(2)%dat,source=dbadatai("*B33193",50))
+!assemble data and attribute
+metaanddatav(1)%dataattrv%dataattr(2)%attrv=attrv
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! station costant data
+! copy the same metadata setting that here we have constant in time data
+metaanddatav(2)%metadata=metaanddatav(1)%metadata%dbacontextana()
+! create  an etherogeneous ensamble of data
+allocate (metaanddatav(2)%dataattrv%dataattr(2))
+allocate (metaanddatav(2)%dataattrv%dataattr(1)%dat,source=dbadatai("B07030",223))
+allocate (metaanddatav(2)%dataattrv%dataattr(1)%attrv%dcv(0))          ! we do not want attributes
+allocate (metaanddatav(2)%dataattrv%dataattr(2)%dat,source=dbadatac("B01019","My beautifull station"))
+allocate (metaanddatav(2)%dataattrv%dataattr(2)%attrv%dcv(0))          ! we do not want attributes
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! display and put everythings in vol7d
+do i=1,size(metaanddatav)
+  call metaanddatav(i)%display()
+end do
+
+call import (v7d,metaanddatav)
+
+call display(v7d)
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 !chiudo il logger
 call l4f_category_delete(category)
