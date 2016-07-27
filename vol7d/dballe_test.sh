db55bc67d9ff2a18a62e1d159cb573014a5f542a
Paolo Patruno <ppatruno@arpa.emr.it>
2016-07-27 16:42:00 +0200

    #22 still alive

--- vol7d/dballe_test.sh
+++ vol7d/dballe_test.sh
@@ -20,5 +20,5 @@ cmp -b dballe_test_copy1fmem.bufr dballe_test_copy1fmem.test || failed
 
 echo "check  dballe_test2.bufr"
 cmp -b dballe_test2.bufr dballe_test2.test || failed
-cmp -b dballe_test2_memdb.bufr dballe_test2.test || failed
+cmp -b dballe_test2_memdb.bufr dballe_test2_memdb.test || failed
 
